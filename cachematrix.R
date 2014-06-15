6e0e592936cba930633032795051bb6e9a5e7520
Carnivalius <paul.may@btopenworld.com>
2014-06-15 01:10:00 +0100

    Added code to solve and cache previously solved matrices

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    '''
+    Function that creates a matrix that can cache
+    its inverse, to save future computation
+    '''
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(solve) m <<- solve
+    getmean <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    '''
+    This function returns the inverse of the inputted matrix
+    utilising the makeCachematrix() function in order to minimise
+    multiple computations. i.e. It caches and retrieves matrixs
+    '''    
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
